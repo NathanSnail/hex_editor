c3ecbae125ded3d750a35cf017ec60538c97eb1d
Nathan <47441022+NathanSnail@users.noreply.github.com>
2026-03-02 12:23:24 +1300

    set up section registry system

--- hex_language/src/section.rs
+++ hex_language/src/section.rs
@@ -1,4 +1,4 @@
-use std::{cell::Cell, ops::Range};
+use std::{cell::Cell, collections::HashMap, ops::Range};
 
 use thiserror::Error;
 use zerocopy::FromBytes;
@@ -9,26 +9,41 @@ pub enum CursorError {
     OutOfBounds,
 }
 
-pub struct Ranged<'section, T> {
-    section: &'section Section<'section>,
+#[derive(Copy, Clone, Default, Hash, PartialEq, Eq)]
+pub struct SectionID(usize);
+
+pub struct Ranged<T> {
+    section: SectionID,
     range: Range<usize>,
     value: T,
 }
 
-pub struct Section<'bytes> {
-    cursor: Cell<usize>,
-    bytes: &'bytes [u8],
+impl<T> Ranged<T> {
+    pub fn map<U>(self, f: impl FnOnce(T) -> U) -> Ranged<U> {
+        Ranged {
+            section: self.section,
+            range: self.range.clone(),
+            value: f(self.value),
+        }
+    }
 }
 
-impl<'bytes> Section<'bytes> {
-    pub fn read(&self, amount: usize) -> Option<Ranged<'_, &[u8]>> {
+#[derive(Clone)]
+pub struct Section {
+    cursor: Cell<usize>,
+    bytes: Box<[u8]>,
+    id: SectionID,
+}
+
+impl Section {
+    pub fn read(&self, amount: usize) -> Option<Ranged<&[u8]>> {
         let pos = self.get_cursor();
         let end = pos + amount;
         self.set_cursor(end).ok()?;
         let range = pos..end;
         let value = &self.bytes[range.clone()];
         Some(Ranged {
-            section: &self,
+            section: self.id,
             range,
             value,
         })
@@ -54,11 +69,33 @@ impl<'bytes> Section<'bytes> {
         }
     }
 
-    pub fn new(bytes: &'bytes [u8]) -> Self {
+    pub fn new(bytes: Box<[u8]>, id: SectionID) -> Self {
         Self {
-            bytes,
+            bytes: bytes,
             cursor: 0.into(),
+            id: id,
         }
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct SectionRegistry {
+    next_id: SectionID,
+    sections: HashMap<SectionID, Section>,
+}
+
+impl SectionRegistry {
+    pub fn new_section(&mut self, bytes: Box<[u8]>) -> &Section {
+        let id = self.next_id;
+        self.next_id.0 += 1;
+        self.sections.insert(id, Section::new(bytes, id));
+        self.sections
+            .get(&id)
+            .expect("Inserted section should exist")
+    }
+
+    pub fn get_section(&self, id: SectionID) -> Option<&Section> {
+        self.sections.get(&id)
     }
 }
 
@@ -71,7 +108,9 @@ mod tests {
 
     #[test]
     fn read_bytes() {
-        let section = Section::new(&[0x00, 0x00, 0x01, 0x23, 0xff, 0x01, 0x00, 0x00]);
+        let mut section_registry = SectionRegistry::default();
+        let section = section_registry
+            .new_section(Box::new([0x00, 0x00, 0x01, 0x23, 0xff, 0x01, 0x00, 0x00]));
         let first: u32 = section.read_cast::<U32<BE>>().unwrap().into();
         assert_eq!(first, 0x0123);
         let second: u32 = section.read_cast::<U32<LE>>().unwrap().into();
