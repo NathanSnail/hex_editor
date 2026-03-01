1a01a4d021df198659813271ae8bb4050de0b20c
Nathan <47441022+NathanSnail@users.noreply.github.com>
2026-03-01 16:03:12 +1300

    use lua for the hex language

--- hex_language/src/lib.rs
+++ hex_language/src/lib.rs
@@ -2,13 +2,25 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn create_context() -> mlua::Lua {
+    let context = mlua::Lua::new();
+    let greet = context
+        .create_function(|_, name: String| {
+            println!("Hello {name}");
+            Ok(2)
+        })
+        .unwrap();
+    context.globals().set("greet", greet).unwrap();
+    context
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let context = create_context();
+        assert_eq!(context.load("greet('foo')").eval::<u32>().unwrap(), 2)
     }
 }
