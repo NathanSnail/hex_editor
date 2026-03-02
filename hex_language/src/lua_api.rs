0c866892fd7d9fef14dc05228b161ae87d2ff777
Nathan <47441022+NathanSnail@users.noreply.github.com>
2026-03-03 12:45:36 +1300

    scriptable registry

---
+++ hex_language/src/lua_api.rs
@@ -0,0 +1,49 @@
+use mlua::{AsChunk, Chunk, Lua};
+
+use crate::section::SectionRegistry;
+
+pub struct ScriptableRegistry {
+    pub registry: SectionRegistry,
+    lua: Lua,
+}
+
+impl ScriptableRegistry {
+    fn add_api(lua: &Lua) {
+        let greet = lua
+            .create_function(|_, name: String| {
+                println!("Hello {name}");
+                Ok(2)
+            })
+            .unwrap();
+
+        lua.globals().set("greet", greet).unwrap();
+    }
+
+    pub fn new(registry: SectionRegistry) -> Self {
+        let lua = Lua::new();
+        ScriptableRegistry::add_api(lua);
+        Self { registry, lua }
+    }
+
+    pub fn load<'a>(&self, src: impl AsChunk + 'a) -> Chunk<'a> {
+        self.lua.load(src)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_fn() {
+        let registry = SectionRegistry::default();
+        let scriptable_registry = ScriptableRegistry::new(registry);
+        assert_eq!(
+            scriptable_registry
+                .load("greet('foo')")
+                .eval::<u32>()
+                .unwrap(),
+            2
+        )
+    }
+}
