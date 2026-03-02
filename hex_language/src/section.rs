660ec6a957ad8de95b12d88abd6bf2132fd9aaaf
Nathan <47441022+NathanSnail@users.noreply.github.com>
2026-03-02 16:27:17 +1300

    make reading ranged

--- hex_language/src/section.rs
+++ hex_language/src/section.rs
@@ -1,4 +1,8 @@
-use std::{cell::Cell, collections::HashMap, ops::Range};
+use std::{
+    cell::Cell,
+    collections::HashMap,
+    ops::{Deref, Range},
+};
 
 use thiserror::Error;
 use zerocopy::{FromBytes, Immutable, IntoBytes};
@@ -9,9 +13,10 @@ pub enum CursorError {
     OutOfBounds,
 }
 
-#[derive(Copy, Clone, Default, Hash, PartialEq, Eq)]
+#[derive(Copy, Clone, Default, Hash, PartialEq, Eq, Debug)]
 pub struct SectionID(usize);
 
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Ranged<T> {
     section: SectionID,
     range: Range<usize>,
@@ -25,6 +30,25 @@ impl<T> Ranged<T> {
             range: self.range.clone(),
             value: f(self.value),
         }
+    }
+
+    pub fn range(&self) -> &Range<usize> {
+        &self.range
+    }
+
+    pub fn value(&self) -> &T {
+        &self.value
+    }
+
+    pub fn section(&self) -> SectionID {
+        self.section
+    }
+}
+
+impl<T> Deref for Ranged<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.value
     }
 }
 
@@ -53,10 +77,12 @@ impl Section {
 
     /// Reads a `T` from the current cursor position and advances the cursor, does nothing if it
     /// cannot be read
-    pub fn read_cast<T: FromBytes>(&self) -> Option<T> {
+    pub fn read_cast<T: FromBytes>(&self) -> Option<Ranged<T>> {
         self.read(size_of::<T>()).map(|x| {
-            T::read_from_bytes(x.value)
-                .expect("Ranged data should always be the correct size for T")
+            x.map(|value| {
+                T::read_from_bytes(value)
+                    .expect("Ranged data should always be the correct size for T")
+            })
         })
     }
 
@@ -130,8 +156,8 @@ impl SectionRegistry {
 
 #[cfg(test)]
 mod tests {
-    use zerocopy::U32;
     use zerocopy::byteorder::{BE, LE};
+    use zerocopy::{U16, U32};
 
     use super::*;
 
@@ -140,9 +166,9 @@ mod tests {
         let mut section_registry = SectionRegistry::default();
         let section = section_registry
             .new_section(Box::new([0x00, 0x00, 0x01, 0x23, 0xff, 0x01, 0x00, 0x00]));
-        let first: u32 = section.read_cast::<U32<BE>>().unwrap().into();
+        let first: u32 = (*section.read_cast::<U32<BE>>().unwrap()).into();
         assert_eq!(first, 0x0123);
-        let second: u32 = section.read_cast::<U32<LE>>().unwrap().into();
+        let second: u32 = (*section.read_cast::<U32<LE>>().unwrap()).into();
         assert_eq!(second, 0x01ff);
         assert_eq!(section.read_cast::<U32<LE>>(), None)
     }
@@ -157,6 +183,15 @@ mod tests {
         assert_eq!(section.write_cast(1234), Err(CursorError::OutOfBounds));
 
         section.set_cursor(0).unwrap();
-        assert_eq!(value, section.read_cast::<U32<LE>>().unwrap());
+        assert_eq!(value, *section.read_cast::<U32<LE>>().unwrap());
+    }
+
+    #[test]
+    fn read_ranged() {
+        let mut section_registry = SectionRegistry::default();
+        let section = section_registry.new_section(Box::new([0x00, 0x00, 0x00, 0x00]));
+
+        assert_eq!(*section.read_cast::<U16<LE>>().unwrap().range(), 0..2);
+        assert_eq!(*section.read_cast::<U16<LE>>().unwrap().range(), 2..4);
     }
 }
